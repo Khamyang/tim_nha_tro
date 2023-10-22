9333af47da00f2c24bee6196ec8985dbc8b885f3
Ok YPS@DESKTOP-725A6LF <5xyoosovn@gmail.com>
2023-10-22 19:57:20 +0700

    update

--- layout/header.php
+++ layout/header.php
@@ -84,7 +84,6 @@ $curPageName = substr($_SERVER["SCRIPT_NAME"], strrpos($_SERVER["SCRIPT_NAME"], 
               <a class="nav-link text-dark h5" href="" target="blank"><i class="fab fa-facebook-square"></i></a>
             </li>
           </ul> -->
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, qui.</p>
         </div>
       </div>
     </nav>
