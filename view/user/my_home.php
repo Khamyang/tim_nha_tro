9b7c21bb3cf75240252a8785557c7023c7d6d901
khamhak100 <khamhak100@gmail.com>
2023-10-26 09:03:38 +0700

    add link to my_home

---
+++ view/user/my_home.php
@@ -0,0 +1,50 @@
+<div class="mt-3">
+    <div class="container">
+        <div class="row">
+            <div class="content_header alert-success p-3 d-flex align-items-lg-center rounded">
+                <div style="width: 50%;">
+                    <span>Nhà trọ đang có hiện tại</span>
+                </div>
+                <div style="width: 50%;">
+                    <input type="text" class="form-control rounded-pill" name="search" id="search" placeholder="Tìm kiếm">
+                </div>
+                
+            </div>
+        </div>
+        <div class="row pt-3 pb-3" id="content_all">
+            <div class="col-sm-3 d-flex justify-content-center">
+                <div class="card d-flex align-items-lg-center " style="width: 18rem;">
+                    <img src="./image/patuxay.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <textarea name="" id="" cols="30" rows="5" class="form-control"></textarea>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-3 d-flex justify-content-center">
+                <div class="card d-flex align-items-lg-center " style="width: 18rem;">
+                    <img src="./image/patuxay.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <textarea name="" id="" cols="30" rows="5" class="form-control"></textarea>
+                    </div>
+                </div>
+            </div>
+             <div class="col-sm-3 d-flex justify-content-center">
+                <div class="card d-flex align-items-lg-center " style="width: 18rem;">
+                    <img src="./image/patuxay.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <textarea name="" id="" cols="30" rows="5" class="form-control"></textarea>
+                    </div>
+                </div>
+            </div>
+             <div class="col-sm-3 d-flex justify-content-center">
+                <div class="card d-flex align-items-lg-center " style="width: 18rem;">
+                    <img src="./image/patuxay.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <textarea name="" id="" cols="30" rows="5" class="form-control"></textarea>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+</div>
